ef6b374c357d2c29789944e372a4f74e041ab034
Ivan Podymov <p0dymovivan@yandex.ru>
2024-03-26 19:48:46 +0300

    registration

--- account/views.py
+++ account/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -13,12 +14,35 @@ def user_login(request):
                 request, username=cd["username"], password=cd["password"]
             )
             if user is not None:
-                login(request, user)
-                return HttpResponse("You are now logged in!")
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponse("Authenticated successfully")
+                else:
+                    return HttpResponse("Disabled account")
             else:
-                return HttpResponse("Incorrect username and/or password.")
-        else:
-            return HttpResponse("Invalid login.")
+                return HttpResponse("Invalid login")
     else:
         form = LoginForm()
     return render(request, "account/login.html", {"form": form})
+
+
+@login_required
+def dashboard(request):
+    return render(request, "account/dashboard.html", {"section": "dashboard"})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST) 
+        if user_form.is_valid():
+        # Создать новый объект пользователя,
+        # но пока не сохранять его
+            new_user = user_form.save(commit=False) # Установить выбранный пароль new_user.set_password(
+            user_form.cleaned_data['password'] # Сохранить объект User
+            new_user.save()
+            return render(request,
+            'account/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+        return render(request, 'account/register.html',
+        {'user_form': user_form})
